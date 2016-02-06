5baf6a277a01ff80b64c7a04cb0338109840cc97
Steve Pentler <stevepentler@gmail.com>
2016-02-05 19:04:08 -0700

    unregistered guest adds concert to cart

--- test/integration/guest_adds_concert_to_cart_test.rb
+++ test/integration/guest_adds_concert_to_cart_test.rb
@@ -35,4 +35,37 @@ class GuestAddsConcertToCartTest < ActionDispatch::IntegrationTest
     assert page.has_content?("#{concert.price * 2}")
     assert page.has_content?("Checkout!")
   end 
+
+  test "unregistered guest adds concert to cart" do 
+    venue = create(:venue)
+    concert = create(:concert, venue: venue)
+
+    visit venue_concert_path(concert.venue.url, concert.url)
+    assert_equal venue_concert_path(concert.venue.url, concert.url), current_path
+    
+    assert page.has_content?(concert.date)
+    assert page.has_content?("Logo")
+    assert page.has_content?(concert.band)
+    assert page.has_content?(concert.venue.name)
+    assert page.has_content?(concert.price)
+    assert page.has_content?("Quantity")
+
+    fill_in "order[quantity]", with: 2
+
+    click_button("Add to Cart")
+
+    assert_equal cart_path, current_path
+
+    assert page.has_content?(concert.date)
+    assert page.has_content?("Logo")
+    assert page.has_content?(concert.band)
+    assert page.has_content?(concert.venue.name)
+    assert page.has_content?(concert.price)
+    assert page.has_content?("Quantity")
+    assert page.has_content?(concert.price)
+
+    assert page.has_content?("2")
+    assert page.has_content?("#{concert.price * 2}")
+    assert page.has_content?("Checkout!")
+  end 
 end
