30d7d4a128085f67945da2af020021cbc7704919
John Slota <john.slota@gmail.com>
2016-02-05 23:45:13 -0700

    started test

--- test/integration/guest_adds_concert_to_cart_test.rb
+++ test/integration/guest_adds_concert_to_cart_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class GuestAddsConcertToCartTest < ActionDispatch::IntegrationTest
-  test "registered guest adds concert to cart" do 
+  test "registered guest adds concert to cart" do
     venue = create(:venue)
     concert = create(:concert, venue: venue)
     registered_user = create(:user, role: 0)
@@ -9,7 +9,7 @@ class GuestAddsConcertToCartTest < ActionDispatch::IntegrationTest
 
     visit venue_concert_path(concert.venue.url, concert.url)
     assert_equal venue_concert_path(concert.venue.url, concert.url), current_path
-    
+
     assert page.has_content?(concert.date)
     assert page.has_content?("Logo")
     assert page.has_content?(concert.band)
@@ -34,15 +34,15 @@ class GuestAddsConcertToCartTest < ActionDispatch::IntegrationTest
     assert page.has_content?("2")
     assert page.has_content?("#{concert.price * 2}")
     assert page.has_content?("Checkout!")
-  end 
+  end
 
-  test "unregistered guest adds concert to cart" do 
+  test "unregistered guest adds concert to cart" do
     venue = create(:venue)
     concert = create(:concert, venue: venue)
 
     visit venue_concert_path(concert.venue.url, concert.url)
     assert_equal venue_concert_path(concert.venue.url, concert.url), current_path
-    
+
     assert page.has_content?(concert.date)
     assert page.has_content?("Logo")
     assert page.has_content?(concert.band)
@@ -67,5 +67,45 @@ class GuestAddsConcertToCartTest < ActionDispatch::IntegrationTest
     assert page.has_content?("2")
     assert page.has_content?("#{concert.price * 2}")
     assert page.has_content?("Checkout!")
-  end 
+  end
+
+  test "registered guest can buy tickets" do
+    venue = create(:venue)
+    concert = create(:concert, venue: venue)
+    registered_user = create(:user, role: 0)
+    ApplicationController.any_instance.stubs(:current_user).returns(registered_user)
+
+    visit venue_concert_path(concert.venue.url, concert.url)
+    assert_equal venue_concert_path(concert.venue.url, concert.url), current_path
+
+    assert page.has_content?(concert.date)
+    assert page.has_content?("Logo")
+    assert page.has_content?(concert.band)
+    assert page.has_content?(concert.venue.name)
+    assert page.has_content?(concert.price)
+    assert page.has_content?("Quantity")
+
+    fill_in "order[quantity]", with: 2
+
+    click_button("Add to Cart")
+
+    assert_equal cart_path, current_path
+
+    assert page.has_content?(concert.date)
+    assert page.has_content?("Logo")
+    assert page.has_content?(concert.band)
+    assert page.has_content?(concert.venue.name)
+    assert page.has_content?(concert.price)
+    assert page.has_content?("Quantity")
+    assert page.has_content?(concert.price)
+
+    assert page.has_content?("2")
+    assert page.has_content?("#{concert.price * 2}")
+    assert page.has_content?("Checkout!")
+
+    click_link("Checkout!")
+
+    save_and_open_page
+  end
+
 end
