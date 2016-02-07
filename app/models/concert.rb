7e52daab4c8f3e6d6d13812e694852c82491c638
Steve Pentler <stevepentler@gmail.com>
2016-02-07 12:05:25 -0700

    Restricts concert logo size

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -4,10 +4,8 @@ class Concert < ActiveRecord::Base
   has_many :concert_orders
   has_many :orders, through: :concert_orders
 
-  validates :logo, attachment_presence: true
-  validates_with AttachmentPresenceValidator, attributes: :logo
   validates_with AttachmentSizeValidator, attributes: :logo, less_than: 1.megabytes
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://static4.businessinsider.com/image/51783cd26bb3f7c826000009/this-is-the-best-way-to-buy-concert-tickets-on-your-iphone.jpg"
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
 
   def generate_url
