a3093f9d4f543e768932afbc1d867b4eccd05ae3
John Slota <john.slota@gmail.com>
2016-02-07 13:14:37 -0700

    platform admin can approve or decline store

--- test/factories.rb
+++ test/factories.rb
@@ -26,7 +26,7 @@ FactoryGirl.define do
   end
 
   factory :user do
-    username "Dexter.Fowler"
+    username { Faker::Company.name }
     password "pass"
     picture "http://l1.yimg.com/bt/api/res/1.2/W_VfC5faktmNRESq5jg6fg--/YXBwaWQ9eW5ld3NfbGVnbztpbD1wbGFuZTtxPTc1O3c9NjAw/http://media.zenfs.com/en/person/Ysports/dexter-fowler-baseball-headshot-photo.jpg"
   end
---
+++ test/integration/platform_admin_approves_venue_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class PlatformAdminApprovesVenueTest < ActionDispatch::IntegrationTest
+  test 'platform admin can approve' do
+    user = create(:user,role: 2)
+    user_2 = create(:user)
+    venue = create(:venue, user_id: user_2.id)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    visit admin_venues_path
+
+    assert current_path, admin_venues_path
+    assert page.has_content?(venue.name)
+    assert page.has_content?("Pending")
+
+    click_on("Pending")
+
+    assert current_path, edit_admin_venue_path(venue)
+
+    check('Approved')
+    click_on("Update Venue")
+
+    assert current_path, admin_venues_path
+
+    assert page.has_content?("#{venue.name} Updated!")
+  end
+end
