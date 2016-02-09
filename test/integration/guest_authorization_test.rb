2a97ae378b40b8c7f15372338f9bab078aaaaf5b
Steve Pentler <stevepentler@gmail.com>
2016-02-08 17:19:25 -0700

    Guest priveleges tests

---
+++ test/integration/guest_authorization_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+class GuestAuthorizationTest < ActionDispatch::IntegrationTest
+  test "guest can login" do 
+    venue = create(:venue)
+    concert = create(:concert)
+    venue.concerts << concert
+
+    visit new_user_path
+    assert_equal new_user_path, current_path
+
+    fill_in "user[username]", with: "SteveAustin316@hotmail.com"
+    fill_in "user[password]", with: "316"
+
+    within('form') do
+      click_on("Create Account")
+    end
+
+    assert_equal user_path(User.last.id), current_path
+    assert page.has_content?("SteveAustin316@hotmail.com")
+    assert page.has_content?("Completed Orders")
+  end 
+
+  test "guest views homepage" do 
+    venue = create(:venue)
+    concert = create(:concert)
+    venue.concerts << concert
+
+    visit root_path
+
+    assert page.has_content?(venue.name)
+    assert page.has_content?(concert.band)
+  end
+
+  test "guest views venue show page" do 
+    venue = create(:venue)
+    concert = create(:concert, venue: venue)
+    venue.concerts << concert
+
+    visit venue_path(venue.url)
+
+    assert page.has_content?(venue.name)
+    assert page.has_content?(concert.band)
+    refute page.has_content?("Edit")
+    refute page.has_content?("Remove")
+    refute page.has_content?("Add an Admin")
+
+  end 
+
+  test "guest views venue_concert show page" do 
+    venue = create(:venue)
+    concert = create(:concert, venue: venue)
+    venue.concerts << concert
+
+    visit venue_concert_path(venue.url, concert.url)
+
+    assert page.has_content?(venue.name)
+    assert page.has_content?(concert.band)
+    assert page.has_content?("Quantity")
+    refute page.has_content?("Edit")
+    refute page.has_content?("Remove")
+  end 
+end
