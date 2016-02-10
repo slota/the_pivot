ea243babe67ef556d4510c07d70602a50a364c84
Hector Huertas <hectorhuertas@gmail.com>
2016-02-10 15:19:41 -0700

    Basic filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,23 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def search
+    # @concerts = Concert.where("band LIKE '%#{params[:search][:Band]}%'")
+    # @concerts = Concert.active_venues.filter_band(params).filter_city(params).filter_date(params).paginate(page: params[:page], per_page: 8)
+    # @concerts = Concert.active_venues.filter_date(params).filter_band(params).paginate(page: params[:page], per_page: 8)
+    @concerts = Concert.active_venues
+    @concerts = @concerts.band(params) if params[:search][:Band]
+  @concerts = @concerts.date(params) if params[:search][:Date]
+@concerts = @concerts.genre(params) if params[:search][:Genre]
+@concerts = @concerts.paginate(page: params[:page], per_page: 8)
+                       #  .city(params)
+    render :home
+  end
+
+  private
+
+      # def paginate
+      #   paginate(page: params[:page], per_page: 8)
+      # end
 end
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -5,6 +5,27 @@ class Concert < ActiveRecord::Base
   has_many :orders, through: :concert_orders
   belongs_to :category
 
+  scope :band, ->(params) {
+    band = params[:search][:Band]
+    where("band LIKE '%#{band}%'")
+  }
+  scope :date, ->(params) {
+    text_date = params[:search][:Date]
+      where(date: Date.strptime(text_date, "%Y-%m-%d")) unless text_date.empty?
+    # if !text_date.empty?
+    #   date = Date.strptime(text_date, "%Y-%m-%d")
+    #   where(date: date)
+    # end
+  }
+  scope :city, ->(params) {
+    city = params[:search][:City]
+    where(venue_id: Venue.find_by(city: city))
+  }
+  scope :genre, ->(params) {
+    genre = params[:search][:Genre]
+    where(category_id: Category.find_by(description: genre))
+  }
+
   validates_with AttachmentSizeValidator, attributes: :logo, less_than: 1.megabytes
   has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://static4.businessinsider.com/image/51783cd26bb3f7c826000009/this-is-the-best-way-to-buy-concert-tickets-on-your-iphone.jpg"
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
@@ -12,4 +33,42 @@ class Concert < ActiveRecord::Base
   def generate_url
     self.url = "#{date}-#{band.parameterize}"
   end
+
+
+
+  def self.filter_band(params)
+    band = params
+    # band = params[:search][:Band]
+    if band
+      relation.where("band LIKE '%#{band}%'")
+    else
+      relation
+    end
+  end
+
+  def self.filter_date(params)
+    text_date = params[:search][:Date]
+    date = Date.strptime(text_date, "%Y-%m-%d")
+    if date
+      # relation.where("date LIKE '%#{date}%'")
+      relation.where(date: date)
+      # binding.pry
+    else
+      relation
+    end
+  end
+
+  def self.filter_city(params)
+    city = params[:search][:City]
+    if city == 99
+      relation.where("band LIKE '%#{city}%'")
+    else
+      relation
+    end
+  end
+
+  def self.active_venues
+    relation.where(venue_id: Venue.where(status: 1))
+  end
+
 end
