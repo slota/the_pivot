4d4011a44aec01aa74e3628b44745497527538e4
John Slota <john.slota@gmail.com>
2016-02-10 13:03:17 -0700

    add an admin button removed for business admins

--- test/integration/business_admin_manage_concerts_test.rb
+++ test/integration/business_admin_manage_concerts_test.rb
@@ -8,7 +8,6 @@ class BusinessAdminManageConcertsTest < ActionDispatch::IntegrationTest
     ApplicationController.any_instance.stubs(:current_user).returns(user)
 
     visit venue_path(venue.url)
-    save_and_open_page
 
     refute page.has_content?("Add an Admin")
 
