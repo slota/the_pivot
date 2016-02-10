c64a9915b17ab3dfeb5c871b241d2bf7faa815fb
Steve Pentler <stevepentler@gmail.com>
2016-02-09 17:41:30 -0700

    Email implementation in progress

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,11 +1,12 @@
 class NotificationController < ApplicationController
   
   def create
-    TicketConfirmation.inform(current_user, params[:email]).deliver_later
-    flash.now[:notice] = "Thank you for your purchase! \nPlease check your email for confirmation."
+    TicketConfirmation.inform(current_user, params[:address]).deliver_later
       if platform_admin?
+        flash.now[:notice] = "Thank you for your purchase! \nPlease check your email for confirmation."
         redirect_to admin_user_path(current_user.id)
       else
+        flash.now[:notice] = "Thank you for your purchase! \nPlease check your email for confirmation."
         redirect_to user_path(current_user.id)
       end
   end
--- app/mailers/ticket_confirmation.rb
+++ app/mailers/ticket_confirmation.rb
@@ -1,12 +1,7 @@
 class TicketConfirmation < ApplicationMailer
   def inform(user, client)
     @user = user
-    mail(to: user.username, subject: "Concert tickets for #{user.username}.")
+    @orders = @user.orders.all
+    mail(to: client, subject: "Concert tickets for #{user.username}.")
+  end
 end
-
-# class FriendNotifier < ApplicationMailer
-#   def inform(user, friend_contact)
-#     @user = user
-#     mail(to: friend_contact, subject: "#{user.name} says you've changed.")
-#   end
-# end
