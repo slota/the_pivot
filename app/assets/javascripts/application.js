f8f4244712af6a9538d9e4f83ef74f40dc5b440f
Steve Pentler <stevepentler@gmail.com>
2016-02-10 11:20:55 -0700

    removes barby and jquery barcode attempts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,21 +39,6 @@ var loaded = function(){
 
 $(document).ready(function() {
     $('select').material_select();
-    $("#barcode").barcode("1234567890128", "ean13");
-    $("#barcode").css({
-      barWidth: 1,
-      barHeight: 50,
-      moduleSize: 5,
-      showHRI: true,
-      addQuietZone: true,
-      marginHRI: 5,
-      bgColor: "#FFFFFF",
-      color: "#000000",
-      fontSize: 10,
-      output: "css",
-      posX: 0,
-      posY: 0
-      })
 });
 
 $(document).on("page:load ready", loaded);
