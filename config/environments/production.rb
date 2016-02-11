1d1c7763c285bf4e4ff7c05a8404fc683ea95e38
Steve Pentler <stevepentler@gmail.com>
2016-02-11 12:46:31 -0700

    AWS changes production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,16 @@ Rails.application.configure do
 #   :bucket => 'ticket-maestro'
 # }
 
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESSS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :url => ':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
   }
-}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
