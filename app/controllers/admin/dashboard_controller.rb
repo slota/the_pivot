c4ead4b93e099b6ab7dd33d992dd651a2e860fa9
John Slota <john.slota@gmail.com>
2016-02-10 23:11:30 -0700

    clean up old pages

--- app/controllers/admin/dashboard_controller.rb
+++
@@ -1,5 +0,0 @@
-class Admin::DashboardController < Admin::BaseController
-  def index
-    @orders = Order.desc_by_date
-  end
-end
--- test/models/order_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class OrderTest < ActiveSupport::TestCase
-  # should validate_presence_of(:status)
-  # should validate_presence_of(:total_price)
-  # should validate_presence_of(:user_id)
-end
