7f0f75b95e6603e5d91865931c9a9c4d8f002f11
John Slota <john.slota@gmail.com>
2016-02-11 11:39:59 -0700

    refactored notifications controller

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -2,6 +2,11 @@ class NotificationController < ApplicationController
 
   def create
     TicketConfirmation.inform(current_user, params[:address]).deliver_later
+    redirect
+  end
+
+  private
+  def redirect
     if platform_admin?
       flash.now[:notice] = "Thank you for your purchase! \nPlease check your email for confirmation."
       redirect_to admin_user_path(current_user.id)
@@ -10,4 +15,6 @@ class NotificationController < ApplicationController
       redirect_to user_path(current_user.id)
     end
   end
+
+
 end
